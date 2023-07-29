d904e168bd8edb112927a25bd33c0399abbe787e
Volodymyr2610 <129291672+Volodymyr2610@users.noreply.github.com>
2023-07-29 18:43:48 +0300

    1

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,14 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const colorCodeRef = document.querySelector('.color');
+const btnRef = document.querySelector('.change-color');
+const bodyRef = document.querySelector('body')
+
+btnRef.addEventListener('click', changeColor);
+
+function changeColor() {
+bodyRef.style.backgroundColor = getRandomHexColor();
+colorCodeRef.textContent = bodyRef.style.backgroundColor;
+};
--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,46 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+// const creatForm = document.querySelector('#controls');
+const btnCreate = document.querySelector('[data-create]');
+const btnDestroy = document.querySelector('[data-destroy]');
+// const inputRef = document.querySelector('input');
+const boxesRef = document.querySelector('#boxes')
+
+btnCreate.addEventListener('click', getAmount);
+btnDestroy.addEventListener('click', destroyBoxes);
+
+function getAmount() {
+  amount = +document.querySelector('#controls input').value;
+  createBoxes(amount);
+}
+
+function createBoxes(amount) {
+  basicSize = 30;
+  fragment = document.createDocumentFragment();
+  for (let i = 0; i < amount; i++) {
+    size = basicSize + i * 10;
+    div = document.createElement('div');
+    div.style.cssText = `width: ${size}px; height: ${size}px; background-color: rgba( ${random()} , ${random()} , ${random()} )`;
+    fragment.appendChild(div);
+  }
+  boxes.appendChild(fragment);
+}
+
+function destroyBoxes() {
+  boxes.innerHTML = '';
+}
+
+function random() {
+  return Math.floor(Math.random() * 256);
+}
+// let boxesAmount = inputHandler;
+
+// inputRef.addEventListener('input', inputHandler);
+
+// function inputHandler(event){
+//   event.preventDefault();
+//   console.log(event.currentTarget.value);
+//  return boxesAmount = event.currentTarget.value;
+// }
